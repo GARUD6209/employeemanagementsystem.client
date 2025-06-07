19294621e038e4ccd79231bd52a132b42ef6a1de
GARUD6209 <harshvardhachandravanshi@gmail.com>
2025-06-07 09:10:15 +0530

    updated models to accommodate samagraIntegration

--- src/models/RegisterUserModel.js
+++ src/models/RegisterUserModel.js
@@ -1,5 +1,23 @@
 export class RegisterUserModel {
-  constructor(firstName = "", lastName = "", email = "", address = "", contact = "", salary = 0, jobRole = "", departmentId = null, trainingRequired = false, role = "") {
+  constructor(
+    firstName = "",
+    lastName = "",
+    email = "",
+    address = "",
+    contact = "",
+    salary = 0,
+    jobRole = "",
+    departmentId = null,
+    trainingRequired = false,
+    role = "",
+    samagraId = 0,
+    FatherName = "",
+    MotherName = "",
+    Gender = "",
+    MaritalStatus = "",
+    DOB = "",
+    District = ""
+  ) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
@@ -9,6 +27,13 @@ export class RegisterUserModel {
     this.jobRole = jobRole;
     this.departmentId = departmentId;
     this.trainingRequired = trainingRequired;
-    this.role = role; // Add role property
+    this.role = role;
+    this.samagraId = samagraId;
+    this.FatherName = FatherName;
+    this.MotherName = MotherName;
+    this.Gender = Gender;
+    this.MaritalStatus = MaritalStatus;
+    this.DOB = DOB;
+    this.District = District;
   }
 }
--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -1,28 +1,37 @@
 export default class Employee {
-    constructor() {
-        this.employeeId = 0;
-        this.firstName = '';
-        this.lastName = '';
-        this.photo = '';
-        this.email = '';
-        this.address = '';
-        this.contact = '';
-        this.emergencyContact = '';
-        this.salary = 0;
-        this.jobRole = '';
-        this.departmentId = 0;
-        this.trainingRequired = false;
-        this.userId = '';
-    }
+  constructor() {
+    this.employeeId = 0;
+    this.firstName = "";
+    this.lastName = "";
+    this.photo = "";
+    this.email = "";
+    this.address = "";
+    this.contact = "";
+    this.emergencyContact = "";
+    this.salary = 0;
+    this.jobRole = "";
+    this.departmentId = 0;
+    this.trainingRequired = false;
+    this.userId = "";
+    this.samagraId = 0;
+    this.FatherName = "";
+    this.MotherName = "";
+    this.Gender = "";
+    this.MaritalStatus = "";
+    this.DOB = "";
+    this.District = "";
+  }
 
-    static fromJson(json) {
-        const employee = new Employee();
-        Object.assign(employee, {
-            ...json,
-            departmentId: Number(json.departmentId),
-            salary: Number(json.salary),
-            trainingRequired: Boolean(json.trainingRequired)
-        });
-        return employee;
-    }
+  static fromJson(json) {
+    const employee = new Employee();
+    Object.assign(employee, {
+      ...json,
+      departmentId: Number(json.departmentId),
+      salary: Number(json.salary),
+      trainingRequired: Boolean(json.trainingRequired),
+      samagraId: Number(json.samagraId),
+      DOB: json.DOB ? json.DOB.split("T")[0] : "",
+    });
+    return employee;
+  }
 }
